424697e332da3cfd23961f17e266fb017d423d88
itgeeksromm <testgit.com>
2023-09-28 22:42:39 -0400

    tag

--- main.tf
+++ main.tf
@@ -11,7 +11,10 @@ provider "aws" {
   region = "us-east-1"
 }
 
-# resource "aws_instance" "webserver" {
-#   ami = "ami-03a6eaae9938c858c"
-#   instance_type = "t2.micro"
-# }
+resource "aws_instance" "webserver" {
+  ami = "ami-03a6eaae9938c858c"
+  instance_type = "t2.micro"
+    tags = {
+        "Name" : "webserver"
+    }
+}
